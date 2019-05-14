e629820b31c49aee7f1bf5b21db41c206d0f96d3
Noah Ströhle <drnochi.dev@gmail.com>
2019-05-14 15:27:07 +0200

    Serve static prototype via Jersey/Tomcat

--- src/main/java/org/m2n/webapplications2/AppConfig.java
+++ src/main/java/org/m2n/webapplications2/AppConfig.java
@@ -4,4 +4,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class AppConfig extends ResourceConfig {
 
+    public AppConfig() {
+        packages("org.m2n.webapplications2");
+    }
+
 }
--- src/main/java/org/m2n/webapplications2/controllers/HelloWorldController.java
+++ src/main/java/org/m2n/webapplications2/controllers/HelloWorldController.java
@@ -4,7 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/")
+@Path("/hello")
 public class HelloWorldController {
 
     @GET
