b5983f8eaf043c50c03dccee4eda91dba0e2829d
Max Kunzelmann <maxdev@posteo.de>
2019-06-18 16:02:21 +0200

    Display flashcards per category in "show cards"

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -15,6 +15,7 @@ function addCategoryToHtml(category) {
     template.querySelector('h6').innerText = category.tagline ? category.tagline : ''
     template.querySelector('p').innerText = category.description ? category.description : ''
     template.querySelector('.edit-card').href = 'edit-category.html?category=' + category.id
+    template.querySelector('.show-cards').href = 'show-flashcards.html?category=' + category.id
 
     const categoryHtml = document.importNode(template, true)
     const container = document.getElementById('category-container')
---
+++ src/main/webapp/js/show-flashcards.js
@@ -0,0 +1,53 @@
+const categoryId = new URL(location.href).searchParams.get('category')
+
+function getFlashcards() {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.onload = event => resolve(JSON.parse(request.response))
+        request.onerror = event => reject(event)
+        request.open('GET', '/api/flashcard?category=' + categoryId)
+        request.send()
+    })
+}
+
+function getCategory() {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.onload = event => resolve(JSON.parse(request.response))
+        request.onerror = event => reject(event)
+        request.open('GET', '/api/category/' + categoryId)
+        request.send()
+    })
+}
+
+function addFlashcardToHtml(flashcard) {
+    const template = document.getElementById('flashcard-template').content
+    textareas = template.querySelectorAll('textarea')
+    textareas.item(0).innerText = flashcard.question
+    textareas.item(1).innerText = flashcard.answer
+
+    const flashcardHtml = document.importNode(template, true)
+    const container = document.getElementById('flashcard-container')
+    container.insertBefore(flashcardHtml, container.children[container.children.length - 1])
+}
+
+function loadFlashcards() {
+    getFlashcards()
+        .then(value => {
+            for (const flashcard of value) {
+                addFlashcardToHtml(flashcard)
+            }
+        })
+        .catch(reason => console.error(reason))
+}
+
+function loadCategory() {
+    getCategory()
+        .then(value => {
+            document.getElementById('title').innerText = value.name
+        })
+        .catch(reason => console.error(reason))
+}
+
+loadFlashcards()
+loadCategory()
