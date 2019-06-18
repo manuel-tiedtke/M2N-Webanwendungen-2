0708a40d9413a73c95dc701939390d24af400a07
Noah Ströhle <drnochi.dev@gmail.com>
2019-06-18 16:11:23 +0200

    Repaire textarea-expander script

--- src/main/webapp/js/textarea-expander.js
+++ src/main/webapp/js/textarea-expander.js
@@ -1,6 +1,6 @@
 const expandingTextareas = document.getElementsByClassName('expanding')
 for (const textarea of expandingTextareas) {
-    textarea.addEventListener('input', () => {
+    textarea.addEventListener('input', function () {
         if (this.scrollHeight > this.clientHeight) this.style.height = this.scrollHeight + 3 + 'px'
     })
     textarea.style.height = textarea.offsetHeight + 'px'
